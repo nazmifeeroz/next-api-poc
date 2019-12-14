c50520e642d48261b4ca55904fa2d2283b92e077
Nazmi <nazmifeeroz@hotmail.com>
2019-12-14 12:44:37 +0800

    POC - api requests github api

--- pages/api/github.js
+++ pages/api/github.js
@@ -1,7 +1,37 @@
 import fetch from 'isomorphic-unfetch'
+
+const getGithubUserAccessToken = code => {
+  const data = {
+    client_id: process.env.GITHUB_CLIENT_ID,
+    client_secret: process.env.GITHUB_CLIENT_SECRET,
+    code,
+  }
+
+  const options = {
+    method: 'POST',
+    url: 'https://github.com/login/oauth/access_token',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(data),
+  }
+  return fetch(options.url, options).then(async response => {
+    console.log('data', response)
+    const result = await response.json()
+    console.log('result', result)
+  })
+}
+
 export default async (req, res) => {
-  const resp = await fetch('https://loripsum.net/api')
-  const result = await resp.text()
-  console.log('resp', result)
-  res.end(result)
+  const { body, method } = req
+
+  switch (method) {
+    case 'POST':
+      getGithubUserAccessToken(body.code)
+      res.status(200).end('authenticated')
+
+    default:
+      res.status(405).end(`Method ${method} Not Allowed`)
+  }
 }
