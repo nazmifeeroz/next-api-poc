fa72c9f8a96275ffb060e9df5bd1fc0614d61868
Nazmi <nazmifeeroz@hotmail.com>
2019-12-15 22:28:25 +0800

    :sparkles: poc to get user membership

--- pages/api/github.js
+++ pages/api/github.js
@@ -19,9 +19,44 @@ const getGithubUserAccessToken = code => {
   return fetch(options.url, options)
     .then(async response => {
       const result = await response.json()
+      /**
+        result {
+          access_token: <ACCESS_TOKEN>,
+          token_type: 'bearer',
+          scope: 'read:org'
+        }
+       */
       return result
     })
     .catch(err => console.log('err', err))
+}
+
+const checkUserMembership = access_token => {
+  const options = {
+    method: 'GET',
+    url: `https://api.github.com/orgs/${process.env.ORGANISATION}/members`,
+    headers: {
+      'User-Agent': `${process.env.APP_NAME}`,
+      Authorization: `token ${access_token}`,
+    },
+  }
+  return new Promise((resolve, _reject) => {
+    fetch(options.url, options)
+      .then(response => response.json())
+      .then(body => resolve(body))
+  })
+}
+
+const authenticateGithubUser = async code => {
+  const { access_token } = await getGithubUserAccessToken(code)
+  if (!access_token) return { error: 'no access token' }
+
+  const orgMembers = await checkUserMembership(access_token)
+  console.log('orgMembers', orgMembers)
+
+  const isMember = Array.isArray(orgMembers) && orgMembers.length > 0
+
+  return { isMember }
 }
 
 export default async (req, res) => {
@@ -30,11 +65,13 @@ export default async (req, res) => {
   switch (method) {
     case 'POST': {
       const { code } = JSON.parse(body)
-      const result = await getGithubUserAccessToken(code)
+      const result = await authenticateGithubUser(code)
       res.status(200).end(JSON.stringify(result))
     }
 
     default:
-      res.status(405).end(`Method ${method} Not Allowed`)
+      res
+        .status(405)
+        .end(JSON.stringify({ error: `Method ${method} Not Allowed` }))
   }
 }
--- pages/callback.js
+++ pages/callback.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import fetch from 'isomorphic-unfetch'
+import Router from 'next/router'
 
-const CallbackPage = ({ code }) => <div>callback page: {code}</div>
+const CallbackPage = props => {
+  useEffect(() => {
+    // TODO: this is not right as the props are passed as url queries
+    Router.push({
+      pathname: '/',
+      query: props,
+    })
+  }, [])
+
+  return <div>callback page: </div>
+}
 
 CallbackPage.getInitialProps = ({ query }) => {
   const options = {
     method: 'POST',
     body: JSON.stringify({ code: query.code }),
   }
-  fetch(`${process.env.APP_URL}/api/github`, options).then(async response => {
-    const result = await response.json()
-    console.log('result', result)
-  })
-  return { code: query.code }
+  return fetch(
+    `${process.env.APP_URL}/api/github`,
+    options
+  ).then(async response => response.json())
 }
 
 export default CallbackPage
